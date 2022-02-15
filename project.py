ffcc737811995750ef1c51940db868127838c505
David <callejasdavid16@gmail.com>
2022-02-15 18:33:22 +0400

    first update ping to IP addresses in a list object

--- project.py
+++ project.py
@@ -1,5 +1,14 @@
+import os
+
+ip_list = ['8.8.8.8', '8.8.4.4']
+
 def run():
-  pass
+  for ip in ip_list:
+    response = os.popen(f"ping {ip}").read()
+    if "recibidos = 4" in response:
+      print(f"UP {ip} ping successful")
+    else:
+      print(f"Down {ip} Ping unsuccessful")
 
 if __name__ == '__main__':
   run()
